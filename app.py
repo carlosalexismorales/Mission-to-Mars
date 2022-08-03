54cba1b1e5fca59d42181e0c0720e0567b858ea1
Carlos Morales <carlosmorales@Carloss-MacBook-Pro-2.local>
2022-08-02 21:03:15 -0700

    adding and updating multiple files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template
 from flask_pymongo import PyMongo
 import scraping
 
@@ -9,13 +9,19 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+
 @app.route("/scrape")
 def scrape():
     mars = mongo.db.mars
     mars_data = scraping.scrape_all()
-    mars.update_one({}, {"$set": mars_data}, upsert=True)
-    return redirect('/', code=302)
+    mars.update({}, mars_data, upsert=True)
+    return "Scraping Successful!"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
